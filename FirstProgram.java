046c6868c4cac7517d8561d6945815e47a7329c0
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-08-28 22:20:28 -0500

    math

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,6 +1,10 @@
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+        double a = 5.0, b = 10.0, c = 20.0;
+        System.out.println("a+b is " + (a + b));
+        System.out.println("a+b/c is " + (a + b  / c));
+        System.out.println("a*b+c is " + (a * b + c));
+        System.out.println("b/a+c/a is " + (b / a + c / a));
+      }
 
 }
