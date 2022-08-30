4b293066e55ac4c44cc181fb2e53239dccd39427
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-08-29 21:43:40 -0500

    increment test

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,10 +1,11 @@
 public class FirstProgram {
     public static void main(String[] args) {
-        double a = 5.0, b = 10.0, c = 20.0;
-        System.out.println("a+b is " + (a + b));
-        System.out.println("a+b/c is " + (a + b  / c));
-        System.out.println("a*b+c is " + (a * b + c));
-        System.out.println("b/a+c/a is " + (b / a + c / a));
+        int i = 0, j;
+        
+        i = i++;
+        System.out.println("i = " + i);
+        
+        j = i++ + i;
+        System.out.println("i = " + i + ", j = " + j);
       }
-
 }
