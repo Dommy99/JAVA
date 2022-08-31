25846da594e489674690ff653ba1a4cdc11b9c5c
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-08-31 15:54:34 -0500

    obj practice

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,10 +1,9 @@
 public class FirstProgram {
     public static void main(String[] args) {
-        int x = 16;
-        double y = Math.sqrt(x);
-        System.out.println("Square root of " + x + " is " + y);
-    
-        double z = Math.sin(Math.PI / 2);
-        System.out.println("Sine of pi/2 is " + z);
+        StringBuffer sb1 = new StringBuffer("Hello");
+        StringBuffer sb2 = null;
+        sb2 = sb1;
+        sb2.append(" World");
+        System.out.println(sb1);
       }
 }
