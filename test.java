a8ea838ee92ea78545e18e35cd7ab46ff11beb6d
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-09-03 22:20:31 -0500

    java test

---
+++ test.java
@@ -0,0 +1,28 @@
+public class test {
+    private int itemCode;
+  private String title;
+  public BookMultiConstructor(int newItemCode, String newTitle) {
+    setItemCode(newItemCode);
+    setTitle(newTitle);
+  }
+  public BookMultiConstructor(String newTitle) {
+    setItemCode(0);
+    setTitle(newTitle);
+  }
+  public int getItemCode() {
+    return itemCode;
+  }
+  public void setItemCode (int newItemCode) {
+    if (newItemCode > 0) itemCode = newItemCode; 
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle (String newTitle) {
+    title = newTitle;
+  }
+  public void display() {
+    System.out.println(itemCode + " " + title);
+  }
+    
+}
