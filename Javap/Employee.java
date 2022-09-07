4f3c3c30af03a6faffe207c27f0ad4ded0e3de45
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-09-06 21:35:46 -0500

    using this to clean up code

--- Javap/Employee.java
+++ Javap/Employee.java
@@ -6,13 +6,23 @@ public class Employee {
   private int dept;
   private double payRate;
   
-  public Employee() { }
-  
-  public Employee(String firstName, String lastName, 
-        int dept, double payRate) {
+  public Employee() {
+  }
+  public Employee(String firstName, String lastName) {
     setFirstName(firstName);
     setLastName(lastName);
+  }
+  public Employee(String firstName,String lastName, int dept) {
+    this(firstName, lastName);
     setDept(dept);
+  }
+  public Employee(String firstName, String lastName, double payRate) {
+    this(firstName, lastName);
+    setPayRate(payRate);
+  }
+  public Employee(String firstName, String lastName, 
+        int dept, double payRate) {
+    this(firstName, lastName, dept);
     setPayRate(payRate);
   }
 
@@ -20,27 +30,27 @@ public class Employee {
 
   public String getFirstName() { return firstName; }
 
-  public void setFirstName(String fn) {
-    firstName = fn;
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
   }
 
   public String getLastName() { return lastName; }
 
-  public void setLastName(String ln) {
-    lastName = ln;
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
   }
 
   public int getDept() { return dept; }
 
-  public void setDept(int dp) {
-    dept = dp;
+  public void setDept(int dept) {
+    this.dept = dept;
   }
 
 
   public double getPayRate() { return payRate; }
 
-  public void setPayRate(double pay) {
-    payRate = pay;
+  public void setPayRate(double payRate) {
+    this.payRate = payRate;
   }
 
   public String getFullName() {
@@ -51,5 +61,6 @@ public class Employee {
     return "Employee " + id + " dept " + dept + " " +
            getFullName() + " paid " + payRate;
   }
+
 
 }
